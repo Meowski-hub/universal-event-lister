b2e31a4e3a66b2ffebb7706c90edbee653fe5377
Meowski-hub <thom@summitvsa.com>
2025-03-24 20:31:10 -0600

    Add loader class

---
+++ includes/class-uel-loader.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Loader class for Universal Event Lister
+ */
+
+if (!defined('ABSPATH')) {
+    die('Direct access not permitted.');
+}
+
+class UEL_Loader {
+    /**
+     * Actions and filters
+     */
+    protected $actions;
+    protected $filters;
+    
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->actions = array();
+        $this->filters = array();
+    }
+    
+    /**
+     * Add action
+     */
+    public function add_action($hook, $component, $callback, $priority = 10, $accepted_args = 1) {
+        $this->actions = $this->add($this->actions, $hook, $component, $callback, $priority, $accepted_args);
+    }
+    
+    /**
+     * Add filter
+     */
+    public function add_filter($hook, $component, $callback, $priority = 10, $accepted_args = 1) {
+        $this->filters = $this->add($this->filters, $hook, $component, $callback, $priority, $accepted_args);
+    }
+    
+    /**
+     * Add a hook to the collection
+     */
+    private function add($hooks, $hook, $component, $callback, $priority, $accepted_args) {
+        $hooks[] = array(
+            'hook'          => $hook,
+            'component'     => $component,
+            'callback'      => $callback,
+            'priority'      => $priority,
+            'accepted_args' => $accepted_args
+        );
+        
+        return $hooks;
+    }
+    
+    /**
+     * Register all hooks
+     */
+    public function run() {
+        // Register filters
+        foreach ($this->filters as $hook) {
+            add_filter(
+                $hook['hook'],
+                array($hook['component'], $hook['callback']),
+                $hook['priority'],
+                $hook['accepted_args']
+            );
+        }
+        
+        // Register actions
+        foreach ($this->actions as $hook) {
+            add_action(
+                $hook['hook'],
+                array($hook['component'], $hook['callback']),
+                $hook['priority'],
+                $hook['accepted_args']
+            );
+        }
+    }
+}
